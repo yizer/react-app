25d6afac0aff3b8d9c7565d3c56c3b427d90dc92
yangzechao <yang.zechao@chinaott.onaliyun.com>
2018-07-19 15:10:59 +0800

    react 环境搭建

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is react</h1>
+      </div>
+    );
+  }
+}
